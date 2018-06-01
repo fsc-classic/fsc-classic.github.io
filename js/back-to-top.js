59febf42ccbfd09ab8d2831e2609bc6f90a23fe5
fsc-classic <38538261+fsc-classic@users.noreply.github.com>
2018-06-01 19:16:01 +0200

    Add files via upload

---
+++ js/back-to-top.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('#scroller').fadeIn();
+        } else {
+            $('#scroller').fadeOut();
+        }
+    });
+
+    $('#scroller').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
+});
